810c581f6d094ed54ec9673bbaf159522ba50321
Chloe Chen <26chloec@tas.edu.tw>
2024-09-24 12:43:58 +0800

    finished agent began maze testing

--- 04-gridworld/04-gridworld/Agent.java
+++ 04-gridworld/04-gridworld/Agent.java
@@ -9,9 +9,166 @@ public class Agent
 {
     // code your agent.
     // instance variables
+    private static Agent m_instance;
+    private int [][] grid;
+    private boolean isWall;
+    private int direction;
+    private int right;
+    private int [] loc;
+    private int x;
+    private int y;
+    private int [] gold;
+    private int gx;
+    private int gy;
+    private boolean atGoal;
     
     // constructor
+    private Agent(){
+        grid = new int [][] { {1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
+                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
+                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
+                    {1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1},           
+                    {1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1},  
+                    {1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1}, 
+                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
+                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
+                    {1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1},
+                    {1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1},  
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}}; 
+        isWall = false;
+        x = 14;
+        y = 4;
+        loc = new int [] {x, y};
+        //1 - grid right, 2 - grid up, 3 - grid left, 4 grid down
+        right = 2;
+        gx = 3;
+        gy = 0;
+        gold = new int[] {gx, gy};
+        atGoal = false;
+        
+        
+    }
+    
+    public static Agent getInstance(){
+        if(m_instance==null){
+            m_instance = new Agent();
+        }
+        
+        return m_instance;
+    }
+    
+    //getter and setter methods
+    public int [][] getGrid(){
+        return grid;
+    }
+    
+    public void setLoc(int a, int b){
+        loc[0] = a;
+        loc[1] = b;
+    }
+    
+    public int locX(){
+        return loc[0];
+    }
+    
+    public int locY(){
+        return loc[1];
+    }
+    
+    public void setG(int a, int b){
+        gold[0] = a;
+        gold[1] = b;
+    }
+    
+    public int goldX(){
+        return loc[0];
+    }
+    
+    public int goldY(){
+        return loc[1];
+    }
+    
+    public boolean getGoal(){
+        return atGoal;
+    }
     
     // add sense decide act methods
+    //sense senses whether or not there is a wall on the right side of the object and to change directions
+    public void sense(){
+        //1 - grid right, 2 - grid up, 3 - grid left, 4 grid down
+        if (right == 1){
+            //checks right
+            if(grid[loc[0]+1][loc[1]] == 0){
+                right = 4;
+            }
+            //checks front
+            else if(grid[loc[0]+1][loc[1]] == 1 && grid[loc[0]][loc[1]+1] == 1){
+                right = 2;
+            }
+        }
+        else if (right == 2){
+            if (grid[loc[0]][loc[1]+1] == 0){
+                right = 1;
+            }
+            else if (grid[loc[0]][loc[1]+1] == 1 && grid[loc[0]-1][loc[1]] == 1){
+                right = 3; 
+            }
+        }
+        else if (right == 3){
+            if (grid[loc[0]-1][loc[1]] == 0){
+                right = 2;
+            }
+            else if (grid[loc[0]-1][loc[1]] == 1 && grid[loc[0]][loc[1]-1] == 1 ){
+                right = 4;
+            }
+        }
+        else if (right == 4){
+            if (grid[loc[0]][loc[1]-1] == 0){
+                right = 3;
+            }
+            else if (grid[loc[0]][loc[1]-1] == 1 && grid[loc[0]+1][loc[1]] == 1){
+                right = 1;
+            }
+        }
+        
+    }
+    
+    //decides if touches treasure
+    public boolean decide(){
+        if (loc[0]==gold[0] && loc[1]==gold[1]){
+            atGoal = true;
+        }
+        else{
+            atGoal = false;
+        }
+        return atGoal;
+    }
+    
+    //checks direction and move the object forward based on direction
+    public void act(){
+        //1 - grid right, 2 - grid up, 3 - grid left, 4 grid down
+        if (right == 1){
+            if (grid[loc[0]][loc[1]+1]!=1){
+                loc[1] = loc [1]+1;
+            }
+        }
+        else if (right == 2){
+            if (grid[loc[0]-1][loc[1]]!=1){
+                loc[0] = loc [0]-1;
+            }
+        }
+        else if (right == 3){
+            if (grid[loc[0]][loc[1]-1]!=1){
+                loc[1] = loc [1]-1;
+            }
+        }
+        else if (right == 2){
+            if (grid[loc[0]+1][loc[1]]!=1){
+                loc[0] = loc [0]+1;
+            }
+        }
+    }
+
     
 }
--- 04-gridworld/04-gridworld/Gridworld.java
+++ 04-gridworld/04-gridworld/Gridworld.java
@@ -7,32 +7,24 @@
  */
 public class Gridworld
 {
+    private int x;
+    private int y;
     
     public Gridworld(){
-        
+       x = Agent.getInstance().locX();
+       y = Agent.getInstance().locY();
     }
     
     // your grid (2d array with your map) goes here.
     public void map(){
         int row =0;
         int col =0;
-        int [][] grid = { {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
-                    {1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,1},           
-                    {1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1},  
-                    {1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1}, 
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
-                    {1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1},
-                    {1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1},  
-                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}}; 
+        int [][] grid = Agent.getInstance().getGrid(); 
         for (int number [] : grid) {
             for (int num: number){
                 if (num == 1){
                     StdDraw.setPenRadius(0.0035);
-                    StdDraw.square(row+2,col+4,0.5);
+                    StdDraw.filledSquare(row+2,col+4,0.5);
                 }
                 else if (num ==0){
                     StdDraw.setPenRadius(0.0005);
@@ -45,12 +37,18 @@ public class Gridworld
         }
     }
     
+    public void objMov(){
+        StdDraw.picture(x+2, y+4, "images/finha.png");
+    }
+    
+
     // code for your Gridworld
     public void start(){
         StdDraw.setCanvasSize();
         StdDraw.setXscale(0,20);
         StdDraw.setYscale(0,20);
         map();
+        objMov();
     }
         
     
